641defc4de660496b742dd15fec10cd90e3e7eab
gdegjygr <malcevartem808@gmail.com>
2023-11-10 14:01:49 +0600

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
 import React, {useState} from "react"
 
 
-// export default function MyApp() {
-//   const[count, setCount] = useState(0)
-
-//   function handleClick(){
-//     setCount(count + 1)
-//   }
-
-//   return (
-//     <div>
-//       <h1>ARASAKA</h1>
-//       <MyButton count = {count} onClick = {handleClick}/>
-//       <MyButton count = {count} onClick = {handleClick}/>
-//     </div>
-//   )
-// };
-
-
-// function MyButton({count, onClick}){
-//   return (
-//     <button onClick = {onClick}>
-//       You have {count} clicks!
-//     </button>
-//   )
-// }
-
-
 export default function MyApp(){
   const[count, setCount] = useState(0)
 
